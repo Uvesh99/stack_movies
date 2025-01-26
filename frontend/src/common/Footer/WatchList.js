0465f46e3028552685314d4b6b888723ab6e4a1c
Himanshu Heda <142437933+HimanshuHeda@users.noreply.github.com>
2025-01-26 22:37:27 +0530

    Create WatchList.js
    by HimanshuHeda

---
+++ frontend/src/common/Footer/WatchList.js
@@ -0,0 +1,10 @@
+const WatchList = () => {
+  return (
+    <div className="container">
+      <h1>Your Watch List</h1>
+      <p>Manage and organize your favorite movies here...</p>
+    </div>
+  );
+};
+
+export default WatchList;
