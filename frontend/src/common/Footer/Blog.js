6874b249d9fad68b080b3c7fcab22e689bb9e410
Himanshu Heda <142437933+HimanshuHeda@users.noreply.github.com>
2025-01-26 22:36:10 +0530

    Blogs
    by HimanshuHeda

---
+++ frontend/src/common/Footer/Blog.js
@@ -0,0 +1,10 @@
+const Blog = () => {
+  return (
+    <div className="container">
+      <h1>Blog</h1>
+      <p>Welcome to the Movie Stack Blog. Stay tuned for updates...</p>
+    </div>
+  );
+};
+
+export default Blog;
