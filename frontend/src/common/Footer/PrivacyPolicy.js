79493a5f1afd9b33e2b3f99f01092350eb467066
Himanshu Heda <142437933+HimanshuHeda@users.noreply.github.com>
2025-01-26 22:35:26 +0530

    Privacy Policy
    by HimanshuHeda

---
+++ frontend/src/common/Footer/PrivacyPolicy.js
@@ -0,0 +1,10 @@
+const PrivacyPolicy = () => {
+  return (
+    <div className="container">
+      <h1>Privacy Policy</h1>
+      <p>At Movie Stack, we value your privacy...</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
