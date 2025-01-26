47866b6da84109623e79e9c9ac99772ff612379c
Himanshu Heda <142437933+HimanshuHeda@users.noreply.github.com>
2025-01-26 22:34:28 +0530

    Terms and Conditions
    by HimanshuHeda

---
+++ frontend/src/common/Footer/TermsOfUse.js
@@ -0,0 +1,10 @@
+const TermsOfUse = () => {
+  return (
+    <div className="container">
+      <h1>Terms of Use</h1>
+      <p>Welcome to Movie Stack. These are our terms of use...</p>
+    </div>
+  );
+};
+
+export default TermsOfUse;
