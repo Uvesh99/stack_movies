b73924220558312afb92b847f0abe64e1e029160
Himanshu Heda <142437933+HimanshuHeda@users.noreply.github.com>
2025-01-26 22:36:51 +0530

    Create FAQ.js
    by HimanshuHeda

---
+++ frontend/src/common/Footer/FAQ.js
@@ -0,0 +1,10 @@
+const FAQ = () => {
+  return (
+    <div className="container">
+      <h1>Frequently Asked Questions</h1>
+      <p>Here are some common questions and answers about Movie Stack...</p>
+    </div>
+  );
+};
+
+export default FAQ;
